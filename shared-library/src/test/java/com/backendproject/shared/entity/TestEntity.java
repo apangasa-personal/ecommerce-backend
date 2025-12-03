0256009d165793fb4623e9065fb8cdd02d42ad7c
ankitpangasa <63389349+apangasa-personal@users.noreply.github.com>
2025-12-03 23:51:28 +0530

    finalized changes for test  and base entity

--- shared-library/src/test/java/com/backendproject/shared/entity/TestEntity.java
+++ shared-library/src/test/java/com/backendproject/shared/entity/TestEntity.java
@@ -1,20 +1,13 @@
 package com.backendproject.shared.entity;
 
-// JPA Imports (needed for @Entity)
 import jakarta.persistence.Entity;
 
-// Lombok Imports
-import lombok.Data; // Provides @Getter, @Setter, @EqualsAndHashCode, @ToString
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-@Entity // <--- Add this import!
-@Data // <--- Replaces individual @Getter, @Setter, @ToString, @EqualsAndHashCode
+@Data
 @NoArgsConstructor
-@AllArgsConstructor
-// Ensure TestEntity extends BaseEntity
+@Entity
 public class TestEntity extends BaseEntity {
-
-    // Test fields if needed, otherwise leave blank.
-    // The class is intentionally simple to test the parent.
 }
